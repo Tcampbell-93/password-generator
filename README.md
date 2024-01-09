e2255cc68ee06b6d202d481bfaf6f63fef7c786b
Tcampbell93 <tcampbell_93@hotmail.com>
2024-01-08 21:52:35 -0800

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # password-generator
+
+## Description
+
+In this project I was given starter code and had to write a function to generate a random password.
+The user can choose to which types of characters they want and between 8 and 128 characters long.
+In this project I learned a lot about the basics of javascript such as for loops and Math.random.
+
+## Usage
+
+When you click the generate password button you will be prompted to choose between 8 and 128 characters.
+Then it will prompt you if you want to add numbers, special characters, and upper case letters.
+Once you have choosen all the prompts your password will show up in the text box.
+
+## Credits
+
+https://www.w3schools.com/css/
+https://developer.mozilla.org/en-US/
+
--- script.js
+++ script.js
@@ -13,14 +13,14 @@ function writePassword() {
     var characters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","p","q","r","s","t","u","v","w","x","y","z"];
     var numbers = [1,2,3,4,5,6,7,8,9,0];
     var special = ["!","@","#","$","&","*","+","="];
-    var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","P","Q","R","S","T","U","V","W","X","Y","Z"];
+    var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","P","Q","R","S","T","U","V","W","X","Y","Z"]; //Adding variables for characters I want to use
 
     var characterLength = prompt("Choose length of password. (between 8 and 128 characters)");
     var includeNumbers = confirm("Do you want to add numbers to your password?");
     var includeSpecial = confirm("Do you want to include special characters in your password?");
-    var includeUppercase = confirm("Do you want to include uppercase letters in your password?");
+    var includeUppercase = confirm("Do you want to include uppercase letters in your password?");    //Giving the user the options to use/not use said characters
 
-    if (characterLength >= 8 && characterLength <= 128) {
+    if (characterLength >= 8 && characterLength <= 128) {       //Validation of correct character length
       var password = "";
 
       for (var i = 0; i < characterLength; i++) {
@@ -36,7 +36,7 @@ function writePassword() {
         var index = Math.floor(Math.random() * uppercase.length);
         password += uppercase[index];
       } else {
-        var index = Math.floor(Math.random() * characters.length);
+        var index = Math.floor(Math.random() * characters.length);            //Originaly had this first and had a hard time figuring out how to exclude other character types and still have correct character length
         password += characters[index];
       }
       }
@@ -44,10 +44,10 @@ function writePassword() {
       return password;
       
       } else {
-        window.alert("Password has to be between 8 and 128 characters.");
-      }
+        window.alert("Password has to be between 8 and 128 characters.");       // adding alert for if character length is incorrect
     }
   }
+}
   
 
 
